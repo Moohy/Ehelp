65802a4e54aada821390e86c7a56f8f51761db1e
moohy <mode99910@gmail.com>
2019-09-22 14:49:21 +1000

    some commenting and documentation

--- Ehelp/view model/EmergencyTypeVC.swift
+++ Ehelp/view model/EmergencyTypeVC.swift
@@ -10,27 +10,40 @@ class EmergencyTypeVC: UIViewController {
 
     
     @IBAction func signoutButton(_ sender: Any) {
+        // dismiss segue
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func police(_ sender: Any) {
+        //choose emergency from emergency enum "police"
         reportViewModel.addEmergency(type: Emergency.police)
+        
+        //call next view controller
         nextView()
     }
     
     @IBAction func fireFighter(_ sender: Any) {
+        //choose emergency from emergency enum "firefighter"
         reportViewModel.addEmergency(type: Emergency.fireFighter)
+        
+        //call next view controller
         nextView()
     }
     
     @IBAction func ambulance(_ sender: Any) {
         reportViewModel.addEmergency(type: Emergency.ambulance)
+        
+        //call next view controller
         nextView()
     }
     
     func nextView(){
         let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "Submission") as! SubmissionVC
+        
+        //pass view model to the instance of viewmodel on the next view controller
         nextVC.reportViewModel = reportViewModel
+        
+        //push and persent
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
 }
